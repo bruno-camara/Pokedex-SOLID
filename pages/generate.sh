7678cab19de467e1db87170ebb97e428fbc08e2c
Quentin Richaud <qrichaud@gmail.com>
2022-11-28 13:52:05 +0100

    Restaure gitlab pages, deleted by accident in previous commit

---
+++ pages/generate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir -p pages/build/TP
+mkdir -p pages/build/presentation
+
+# Make TP subject HTML page
+cd sujet_TP && make && cd ..
+cp -R sujet_TP/sujet.html sujet_TP/ressources pages/build/TP
+
+
+# Make presentation HTML page
+cp -R presentation pages/build/
+
+
+# Copy all static HTML files and assets
+cp -r pages/static/* pages/build
