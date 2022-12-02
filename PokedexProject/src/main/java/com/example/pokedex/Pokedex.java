ce95a9addeac896f3591f324756b660c05dd146a
Quentin Richaud <qrichaud@gmail.com>
2022-12-02 17:54:49 +0100

    update gradle to 7.6

--- PokedexProject/src/main/java/com/example/pokedex/Pokedex.java
+++ PokedexProject/src/main/java/com/example/pokedex/Pokedex.java
@@ -4,6 +4,7 @@ package com.example.pokedex;
 public class Pokedex {
 
     public static void main(String[] args) {
+        System.out.println("Java version " + System.getProperty("java.version"));
         System.out.println("It's working !");
         if (args.length > 0) {
             System.out.println("Vous avez fourni l'argument " + args[0]);
